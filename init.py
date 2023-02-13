017d0a50eae321af879af2ffcecba0f30f0d6b78
mosko <hataya@keio.jp>
2023-02-13 22:05:29 +0900

    update

--- init.py
+++ init.py
@@ -1,105 +1,22 @@
 import pathlib
 
 def pyproject_toml(owner_name: str, repo_name: str):
-    content = f"""
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
-
-[project]
-name = "{repo_name}"
-description = ''
-readme = "README.md"
-requires-python = ">=3.10"
-license = "MIT"
-keywords = []
-authors = [
-    {{ name = "{owner_name}", email = "{owner_name}@users.noreply.github.com" }},
-]
-classifiers = [
-    "Development Status :: 4 - Beta",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Programming Language :: Python :: Implementation :: CPython",
-]
-dependencies = [
-    "rich"
-]
-dynamic = ["version"]
-
-[tool.hatch.envs.default.env-vars]
-PIP_EXTRA_INDEX_URL = "https://pypi.org/simple/"
-
-[project.urls]
-Documentation = "https://{owner_name}.github.io/{repo_name}"
-Issues = "https://github.com/{owner_name}/{repo_name}/issues"
-Source = "https://github.com/{owner_name}/{repo_name}"
-
-[tool.hatch.version]
-path = "{repo_name}/__about__.py"
-
-[tool.hatch.envs.tests]
-dependencies = [
-    "pytest",
-]
-
-[[tool.hatch.envs.test.matrix]]
-python = ["310", ]
-
-[tool.hatch.envs.docs]
-extra-dependencies = [
-    "mkdocs-material"
-]
-[tool.hatch.envs.docs.scripts]
-build = "mkdocs build --clean --strict"
-
-[tool.hatch.build.targets.sdist]
-exclude = [
-    "/.github",
-    "/docs",
-    "/tests",
-]
-    """
-    with open("pyproject.toml", 'w') as f:
-        f.write(content[1:])
+    file = pathlib.Path("pyproject.toml")
+    with file.open('r') as f:
+        content = f.read()
+    content = content.format(owner_name=owner_name, repo_name=repo_name)
+    with file.open('w') as f:
+        f.write(content)
 
 def mkdocs(owner_name: str, repo_name: str):
-    content = f"""
-site_name: {repo_name}
-site_description: ...
-site_author: {owner_name}
-site_url: https://{owner_name}.github.io/{repo_name}
-repo_name: {owner_name}/{repo_name}
-repo_url: https://github.com/{owner_name}/{repo_name}
-edit_uri: blob/main/docs
-
-theme:
-  name: material
-
-  features:
-    - content.code.annotate
-
-markdown_extensions:
-  # mathjax
-  - pymdownx.arithmatex:
-      generic: true
-
-  # syntax highlighting
-  - pymdownx.highlight:
-      anchor_linenums: true
-  - pymdownx.inlinehilite
-  - pymdownx.snippets
-  - pymdownx.superfences
-
-nav:
-  - Home: 'index.md'    
-"""
-    with open("mkdocs.yml", 'w') as f:
-        f.write(content[1:])
+    file = pathlib.Path("mkdocs.yml")
+    with file.open('r') as f:
+        content = f.read()
+    content = content.format(owner_name=owner_name, repo_name=repo_name)
+    with file.open('w') as f:
+        f.write(content)
 
     docs_root = pathlib.Path("docs")
-    docs_root.mkdir()
     with (docs_root / "index.md").open('w') as f:
         f.write(f"# {repo_name}")
 
@@ -118,6 +35,19 @@ def readme(owner_name: str, repo_name: str):
 
 ![pytest](https://github.com/{owner_name}/{repo_name}/workflows/pytest/badge.svg)
 [![document](https://img.shields.io/static/v1?label=doc&message={repo_name}&color=blue)](https://{owner_name}.github.io/{repo_name})
+
+## Installation
+
+```
+hatch create env
+```
+
+or
+
+```
+pip install -U -e .
+```
+
     """
     with open("README.md", 'w') as f:
         f.write(content[1:])
