e5d606f1dac845b3b97d7f5076cae65222258f64
mosko <hataya@keio.jp>
2023-02-13 22:34:38 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PyProject Template
 
-This template automatically create `hatch`-based repository using the given repository name.
+This template automatically create a repository with [hatch](https://hatch.pypa.io/) using a given repository name.
 For example, if you create a repository "foo" with this template and wait a few seconds,
 the repository becomes something like below.
 
@@ -21,3 +21,28 @@ The created repository is ready for unit tests and document generation.
 After cloning the created repository, edit the generated files according to your needs, e.g., appending dependencies in `pyproject.toml`.
 
 To activate GitHub pages, go to `settings/pages/sources` and change it to `GitHub Actions`, so that the documents will automatically created.
+
+To extend this template, fork this template and name it `*-template` to disable the [initializer action](.github/workflows/init.yaml).
+
+The following section is the template of `README.md`.
+
+---
+
+# {repo_name}
+
+**This project is work in progress**
+
+![pytest](https://github.com/{owner_name}/{repo_name}/workflows/pytest/badge.svg)
+[![document](https://img.shields.io/static/v1?label=doc&message={repo_name}&color=blue)](https://{owner_name}.github.io/{repo_name})
+
+## Installation
+
+```
+hatch create env
+```
+
+or
+
+```
+pip install -U -e .
+```
--- init.py
+++ init.py
@@ -2,21 +2,28 @@ import pathlib
 
 def pyproject_toml(owner_name: str, repo_name: str):
     file = pathlib.Path("pyproject.toml")
+
     with file.open('r') as f:
         content = f.read()
+
     content = content.format(owner_name=owner_name, repo_name=repo_name)
+
     with file.open('w') as f:
         f.write(content)
 
 def mkdocs(owner_name: str, repo_name: str):
     file = pathlib.Path("mkdocs.yml")
+
     with file.open('r') as f:
         content = f.read()
+
     content = content.format(owner_name=owner_name, repo_name=repo_name)
+
     with file.open('w') as f:
         f.write(content)
 
     docs_root = pathlib.Path("docs")
+
     with (docs_root / "index.md").open('w') as f:
         f.write(f"# {repo_name}")
 
@@ -24,33 +31,22 @@ def project_dir(repo_name: str):
     root = pathlib.Path(repo_name)
     root.mkdir()
     (root / "__init__.py").touch()
+
     with (root / "__about__.py").open('w') as f:
         f.write("__version__ = '0.0.1'")
 
 def readme(owner_name: str, repo_name: str):
-    content = f"""
-# {repo_name}
+    file = pathlib.Path("README.md")
 
-**This project is work in progress**
+    with file.open('r') as f:
+        content = f.read()
 
-![pytest](https://github.com/{owner_name}/{repo_name}/workflows/pytest/badge.svg)
-[![document](https://img.shields.io/static/v1?label=doc&message={repo_name}&color=blue)](https://{owner_name}.github.io/{repo_name})
+    content = content.split('---')[1]
+    content = content.format(owner_name=owner_name, repo_name=repo_name)
 
-## Installation
+    with file.open('w') as f:
+        f.write(content)
 
-```
-hatch create env
-```
-
-or
-
-```
-pip install -U -e .
-```
-
-    """
-    with open("README.md", 'w') as f:
-        f.write(content[1:])
 
 if __name__ == "__main__":
     import argparse
